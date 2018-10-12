cda9204549d9fecaa3c8a67bc6429811bc817dd7
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-10-12 14:20:04 +0300

    Added send tokens array function

--- contracts/AzbitAirdrop.sol
+++ contracts/AzbitAirdrop.sol
@@ -60,7 +60,11 @@ contract AzbitAirdrop is Ownable {
         _setToken(tokenAddress);
     }
 
-    // Send tokens to beneficiary by owner
+    /**
+    * @dev Send tokens to beneficiary by owner
+    * @param beneficiary The address for tokens withdrawal
+    * @param amount The token amount
+    */
     function sendTokens(
         address beneficiary,
         uint256 amount
@@ -68,8 +72,27 @@ contract AzbitAirdrop is Ownable {
         external
         onlyOwner
     {
-        require(amount <= contractTokenBalance(), "not enough tokens on this contract");
-        require(azbitToken.transfer(beneficiary, amount), "tokens are not transferred");
+        _sendTokens(beneficiary, amount);
+    }
+
+    /**
+    * @dev Send tokens to the array of beneficiaries  by owner
+    * @param beneficiaries The array of addresses for tokens withdrawal
+    * @param amounts The array of tokens amount
+    */
+    function sendTokensArray(
+        address[] beneficiaries, 
+        uint256[] amounts
+    )
+        external
+        onlyOwner
+    {
+        require(beneficiaries.length == amounts.length, "array lengths have to be equal");
+        require(beneficiaries.length > 0, "array lengths have to be greater than zero");
+
+        for (uint256 i = 0; i < beneficiaries.length; i++) {
+            _sendTokens(beneficiaries[i], amounts[i]);
+        }
     }
 
 
@@ -95,5 +118,20 @@ contract AzbitAirdrop is Ownable {
     {
         azbitToken = AzbitTokenInterface(tokenAddress);
         require(contractTokenBalance() >= 0, "The token being added is not ERC20 token");
+    }
+
+    // Helper: send tokens to beneficiary
+    function _sendTokens(
+        address beneficiary, 
+        uint256 amount
+    )
+        internal
+    {
+        require(beneficiary != address(0), "Address cannot be 0x0");
+        require(amount > 0, "Amount cannot be zero");
+        require(amount <= contractTokenBalance(), "not enough tokens on this contract");
+
+        // transfer tokens
+        require(azbitToken.transfer(beneficiary, amount), "tokens are not transferred");
     }
 }
