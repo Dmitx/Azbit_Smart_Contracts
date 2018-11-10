c5b8d0ca59a312256be288582e5ff8a836fa0dd3
Dmitx <37021407+Dmitx@users.noreply.github.com>
2018-11-10 16:46:50 +0300

    Updated smart contract deployment

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,10 +3,15 @@ var Bounty = artifacts.require("./AzbitBounty.sol");
 var Airdrop = artifacts.require("./AzbitAirdrop.sol");
 var Advisors = artifacts.require("./AzbitAdvisors.sol");
 var USResidents = artifacts.require("./AzbitUSResidents.sol");
+var TeamTokenLock = artifacts.require("./AzbitTeamTokenLock.sol");
+var ReservedTokenLock = artifacts.require("./AzbitReservedTokenLock.sol");
 
 const initialSupply = 1e9;
 const tokenName = "Azbit Token";
 const tokenSymbol = "AZ";
+const initialTokenPrice = 120000; // price in USD * 1e9
+const teamAddress = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57";
+const reservedAddress = "0x627306090abaB3A6e1400e9345bC60c78a8BEf57";
 
 module.exports = function(deployer) {
   deployer.deploy(AzbitToken,
@@ -23,5 +28,11 @@ module.exports = function(deployer) {
   }).then(function() {
     return deployer.deploy(USResidents,
       AzbitToken.address);
+  }).then(function() {
+    return deployer.deploy(TeamTokenLock,
+      AzbitToken.address, initialTokenPrice, teamAddress);
+  }).then(function() {
+    return deployer.deploy(ReservedTokenLock,
+      AzbitToken.address, initialTokenPrice, reservedAddress);
   })
 };
